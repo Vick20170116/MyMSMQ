136a65e4898c9030fdd55bbc1c0f6ba01c3f1a82
Michael Fang <yingchih.fang@gmail.com>
2016-08-28 00:28:25 +0800

    add unit test

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyMSMQSender;
+using MyMSMQReceiver;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int sendCount = MyMSMQSender.Program.SendMessageAll();
+            Assert.AreEqual(sendCount, 10);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int sendCount = MyMSMQSender.Program.SendMessageAll();
+            Assert.AreEqual(sendCount>0, true);
+
+            int receiveCount = MyMSMQReceiver.Program.ReceiveMessage();
+            Assert.AreEqual(receiveCount>0, true);
+
+        }
+    }
+}
